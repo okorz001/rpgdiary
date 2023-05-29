b4542b4a09a20eaae76c33480b6265868c180b17
Oscar Korz <okorz001@gmail.com>
2023-05-29 00:41:05 -0700

    Render date

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -34,13 +34,31 @@ const components = {
 }
 
 export default function Article(props: ArticleData) {
+  const date = props.meta.date && renderDate(props.meta.date)
   return (
     <article className="px-2 py-2">
-      {props.meta.date && <div className="mb-1 text-xs">{props.meta.date}</div>}
+      {date && <div className="mb-1 text-xs">{date}</div>}
       <h2 className="text-3xl text-center font-serif">{props.meta.title}</h2>
       <ArticleMetaContext.Provider value={props.meta}>
         <MDXRemote components={components} {...props.mdxProps} />
       </ArticleMetaContext.Provider>
     </article>
   )
+}
+
+function renderDate(date: string) {
+  // new Date uses UTC midnight when parsing a ISO date, which can make the date wrong in local timezone
+  //   > new Date('2023-05-06')
+  //   2023-05-06T00:00:00.000Z
+  // However, explicitly passing year, month, and date uses local midnight which is always right in local timezone
+  //   > new Date(2023, 5, 6)
+  //   2023-06-06T07:00:00.000Z
+  const [match, year, month, day] = /^(\d+)-(\d+)-(\d+)$/.exec(date) || []
+  if (match) {
+    // Date months are [0, 11]
+    return new Date(+year, +month - 1, +day)
+      .toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
+  } else {
+    console.warn(`Invalid date: ${date}`)
+  }
 }
