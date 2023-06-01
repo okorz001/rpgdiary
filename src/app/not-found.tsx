b74f6ecd050404be908ce5785f97384f16cdd9cd
Oscar Korz <okorz001@gmail.com>
2023-05-31 21:17:38 -0700

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFoundPage() {
+  return (
+    <div className="w-full bg-red-500/20">
+      <h2 className="p-4 text-3xl text-center">Page Not Found</h2>
+      <img width="32" height="74" src="/assets/gaspar.png" alt="Old Man Gaspar" className="mx-auto" />
+      <p className="w-fit py-4 mx-auto italic">
+        Why, this is "The End of Time," of course!<br/>
+        <br/>
+        All lost travelers in time wind up here!<br/>
+        Now, where are you from?
+      </p>
+    </div>
+  )
+}
