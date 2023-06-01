7578d5dd8cbd27eca2fc837ca4908edeadad9162
Oscar Korz <okorz001@gmail.com>
2023-05-31 21:28:46 -0700

    Fix build

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
       <h2 className="p-4 text-3xl text-center">Page Not Found</h2>
       <img width="32" height="74" src="/assets/gaspar.png" alt="Old Man Gaspar" className="mx-auto" />
       <p className="w-fit py-4 mx-auto italic">
-        Why, this is "The End of Time," of course!<br/>
+        Why, this is &quot;The End of Time,&quot; of course!<br/>
         <br/>
         All lost travelers in time wind up here!<br/>
         Now, where are you from?
