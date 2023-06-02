c78ab0c72aae92bc544bcec3cf2b0c5d3b834473
Oscar Korz <okorz001@gmail.com>
2023-06-01 19:05:30 -0700

    Make error pages more red

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,7 @@
 
 export default function ErrorPage() {
   return (
-    <div className="w-full bg-red-500/20">
+    <div className="w-full bg-red-500/40">
       <h2 className="p-4 text-3xl text-center">Error</h2>
       <img width="60" height="81" src="/assets/kefka.png" alt="Kefka angry" className="mx-auto" />
       <p className="w-fit py-4 mx-auto italic">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
   return (
-    <div className="w-full bg-red-500/20">
+    <div className="w-full bg-red-500/40">
       <h2 className="p-4 text-3xl text-center">Page Not Found</h2>
       <img width="32" height="74" src="/assets/gaspar.png" alt="Old Man Gaspar" className="mx-auto" />
       <p className="w-fit py-4 mx-auto italic">
