5f2c0d46b6b186d7cabde454185b96deda4ad6f4
Oscar Korz <okorz001@gmail.com>
2023-06-01 19:01:50 -0700

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function ErrorPage() {
+  return (
+    <div className="w-full bg-red-500/20">
+      <h2 className="p-4 text-3xl text-center">Error</h2>
+      <img width="60" height="81" src="/assets/kefka.png" alt="Kefka angry" className="mx-auto" />
+      <p className="w-fit py-4 mx-auto italic">
+        Son of a submariner!
+      </p>
+    </div>
+  )
+}
