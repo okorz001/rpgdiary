4f4306fbdbf85864d0b864a390e1965718933367
Oscar Korz <okorz001@gmail.com>
2023-07-14 16:41:26 -0700

    Fix react type

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,10 @@
-import { type JSXElementConstructor, type ReactNode } from 'react'
+import { type ComponentType, type ReactNode } from 'react'
 
 export type PropsWithChildren = {
   children?: ReactNode
 }
 
-export function createComponent<P>(name: string, delegate: JSXElementConstructor<P>) {
-  // @ts-ignore
+export function createComponent<P>(name: string, delegate: ComponentType<P>) {
   delegate.displayName = name
   return delegate
 }
