7ae9f9d7f992d2b2c8b7e000b8ce614d44323882
Oscar Korz <okorz001@gmail.com>
2023-08-27 12:56:57 -0700

    Add missing file

---
+++ src/components/Note.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from '@/components'
+
+export default function Note(props: PropsWithChildren) {
+  return (
+      <div className="my-4 w-fit mx-auto rounded-xl bg-yellow-300/20 p-2 text-justify">
+        {props.children}
+      </div>
+  )
+}
