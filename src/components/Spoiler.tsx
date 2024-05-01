750e39f3ac196fefa5b8c8c925639fc9144edc55
Oscar Korz <okorz001@gmail.com>
2024-04-30 20:22:37 -0700

    Improve spoiler CSS

--- src/components/Spoiler.tsx
+++ src/components/Spoiler.tsx
@@ -7,11 +7,16 @@ import { PropsWithChildren } from '@/components'
 export default function Spoiler(props: PropsWithChildren) {
   const [visible, setVisible] = useState(false)
   const toggleClick = () => setVisible(!visible)
+
+  // use visible: hidden to suppress pointer events, e.g. clicking links
+  // use opacity for transitions (it doesn't work with visible)
+  const visibleClass = visible ? 'visible opacity-100' : 'invisible opacity-0'
+
   return (
-    <span onClick={toggleClick} className={`
-      cursor-pointer transition-colors ease-in bg-black ${visible ? 'text-white/80' : 'text-black'}
-    `}>
-      {props.children}
+    <span onClick={toggleClick} className="cursor-pointer bg-black text-white/80">
+      <span className={`transition-all ease-in bg-black ${visibleClass}`}>
+        {props.children}
+      </span>
     </span>
   )
 }
